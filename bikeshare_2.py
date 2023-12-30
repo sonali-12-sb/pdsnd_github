f40c6702facd5c644e685aba348ab69c3834237c
Sonali Balsure <balsuresonali7@gmail.com>
2023-12-31 00:21:01 +0530

    feat: Added comments for print statements and updated .py file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -15,6 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+# print statement for printing line
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
@@ -24,7 +25,7 @@ def get_filters():
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
-
+# print statement for printing line
     print('-'*40)
     return city, month, day
 
@@ -48,6 +49,7 @@ def load_data(city, month, day):
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
+# print statement for printing line
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
